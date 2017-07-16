c19c5193b925d1ecdc061e7defdbacef7c66616b
--show-origin <nikersifyyt@gmail.com>
2017-07-16 19:34:52 +0200

    refactor emitter to use es6 class

--- src/internal/emitter.js
+++ src/internal/emitter.js
@@ -1,69 +1,58 @@
 import {wildcard} from './util'
 
-function Bus() {
-	if (!(this instanceof Bus)) {
-		return new Bus()
+class Emitter {
+	constructor() {
+		this.listeners = []
 	}
 
-	this.listeners = []
-	return this
+	on(domain, fn) {
+		if (!domain || !fn) {
+			return this
+		}
+
+		this.listeners.push({
+			domain, fn
+		})
+	}
+
+	off(domain, fn) {
+		if (!domain) {
+			return this
+		}
+
+		const candidates = this.listeners
+			.filter(l => wildcard(domain, l.domain))
+			.filter(l => fn ? l.fn === fn : true)
+
+		for (const c of candidates)		{
+			this.listeners.splice(this.listeners.indexOf(c), 1)
+		}
+	}
+
+	once(domain, fn) {
+		if (!domain || !fn) {
+			return this
+		}
+
+		const tmp = () => {
+			fn()
+			this.off(domain, tmp)
+		}
+		this.on(domain, tmp)
+	}
+
+	emit(domain, payload) {
+		if (!domain) {
+			return this
+		}
+
+		const candidates = this.listeners
+			.filter(l => wildcard(domain, l.domain))
+
+		for (const c of candidates) {
+			c.fn(payload)
+		}
+	}
 }
 
-Bus.prototype.on = function (domain, fn) {
-	if (!domain || !fn) {
-		return this
-	}
-
-	this.listeners.push({
-		domain, fn
-	})
-
-	return this
-}
-
-Bus.prototype.off = function (domain, fn) {
-	if (!domain) {
-		return this
-	}
-
-	const candidates = this.listeners
-		.filter(l => wildcard(domain, l.domain))
-		.filter(l => fn ? l.fn === fn : true)
-
-	for (const c of candidates)		{
-		this.listeners.splice(this.listeners.indexOf(c), 1)
-	}
-
-	return this
-}
-
-Bus.prototype.once = function (domain, fn) {
-	if (!domain || !fn) {
-		return this
-	}
-
-	const tmp = () => {
-		fn()
-		this.off(domain, tmp)
-	}
-	this.on(domain, tmp)
-
-	return this
-}
-
-Bus.prototype.emit = function (domain, payload) {
-	if (!domain) {
-		return this
-	}
-
-	const candidates = this.listeners
-		.filter(l => wildcard(domain, l.domain))
-
-	for (const c of candidates) {
-		c.fn(payload)
-	}
-
-	return this
-}
-
-export default Bus
+export default Emitter
